96e2d1904857463a3dc11116e9b61af34712207d
Ferdaousbna <123882075+Ferdaousbna@users.noreply.github.com>
2023-02-21 14:11:25 +0100

    Add files via upload

---
+++ sceneBuilderFirst/src/main/java/com/example/scenebuilderfirst/HelloController.java
@@ -0,0 +1,41 @@
+package com.example.scenebuilderfirst;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    @FXML
+    private Button ajout;
+
+    @FXML
+    private Button modifié;
+
+    @FXML
+    private Button suppression;
+
+    @FXML
+    void ajouter(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modifier(ActionEvent event) {
+
+    }
+
+    @FXML
+    void supprimer(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
